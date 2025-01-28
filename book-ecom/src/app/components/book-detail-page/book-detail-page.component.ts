3de1e551e94daaca0e78b479f56f08f36d745c2a
Abdul Rehman <arehman8125@gmail.com>
2025-01-28 22:45:09 +0500

    Add book detail page

--- book-ecom/src/app/components/book-detail-page/book-detail-page.component.ts
+++ book-ecom/src/app/components/book-detail-page/book-detail-page.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute} from '@angular/router';
-import { FooterComponent } from "../footer/footer.component";
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-book-detail-page',
@@ -11,18 +11,73 @@ import { FooterComponent } from "../footer/footer.component";
 export class BookDetailPageComponent {
   dataUrl = 'json/books.json';
   bookTitle: string = '';
-  bookData: any = [];
+  bookData: any = {};
+  fullStars: number = 0;
+  halfStars: number = 0;
+  emptyStars: number = 0;
+  numberOfBooks: number = 1;
+
   constructor(private route: ActivatedRoute) {}
+
   ngOnInit() {
     this.route.queryParams.subscribe((param) => {
       this.bookTitle = param['bookTitle'];
-      console.log(this.bookTitle);
       fetch(this.dataUrl)
-       .then((response) => response.json())
-       .then((data) => {
-          this.bookData = data.find((book: { title: string; }) => book.title === this.bookTitle);
-          console.log(this.bookData);
+        .then((response) => response.json())
+        .then((data) => {
+          this.bookData = data.find(
+            (book: { title: string }) => book.title === this.bookTitle
+          );
+          if (this.bookData && this.bookData['stars'] !== undefined) {
+            this.calculateStars(this.bookData['stars']);
+            this.appendStarsHtml();
+          }
         });
     });
+  }
+
+  calculateStars(stars: number) {
+    this.fullStars = Math.floor(stars);
+    this.halfStars = stars % 1 >= 0.5 ? 1 : 0;
+    this.emptyStars = 5 - (this.fullStars + this.halfStars);
+  }
+
+  appendStarsHtml() {
+    let container = document.querySelector('.stars-container');
+
+    // Append full stars
+    for (let i = 0; i < this.fullStars; i++) {
+      const fullStar = document.createElement('span');
+      fullStar.classList.add('star', 'full-star');
+      fullStar.innerHTML = '★';
+      container?.appendChild(fullStar);
+    }
+
+    // Append half stars
+    for (let i = 0; i < this.halfStars; i++) {
+      const halfStar = document.createElement('span');
+      halfStar.classList.add('star', 'half-star');
+      halfStar.innerHTML = '⯨';
+      container?.appendChild(halfStar);
+    }
+
+    // Append empty stars
+    for (let i = 0; i < this.emptyStars; i++) {
+      const emptyStar = document.createElement('span');
+      emptyStar.classList.add('star', 'empty-star');
+      emptyStar.innerHTML = '☆';
+      container?.appendChild(emptyStar);
+    }
+  }
+
+  incrementBooks() {
+    this.numberOfBooks++;
+  }
+  decrementBooks() {
+    if (this.numberOfBooks > 0) {
+      this.numberOfBooks--;
+    } else if (this.numberOfBooks == 0) {
+      this.numberOfBooks = 0;
+    }
   }
 }
