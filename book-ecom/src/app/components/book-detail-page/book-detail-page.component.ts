d58b2e77ff13756e8176d12dd102c4dec4bb0a1c
Abdul Rehman <arehman8125@gmail.com>
2025-02-06 15:28:44 +0500

    Add files for details page

--- book-ecom/src/app/components/book-detail-page/book-detail-page.component.ts
+++ book-ecom/src/app/components/book-detail-page/book-detail-page.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, inject, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  signal,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
 import { BooksService } from '../../services/books/books.service';
@@ -14,9 +21,10 @@ import { CartService } from '../../services/cart/cart.service';
 export class BookDetailPageComponent {
   bookTitle: string = '';
   bookData: Books[] = [];
-  fullStars: number = 0;
-  halfStars: number = 0;
-  emptyStars: number = 0;
+  fullStars = signal<number>(0);
+  halfStars = signal<number>(0);
+  emptyStars = signal<number>(0);
+  starIcons = signal<string[]>([]);
   numberOfBooks: number = 1;
 
   private route = inject(ActivatedRoute);
@@ -29,39 +37,23 @@ export class BookDetailPageComponent {
       this.bookData = this.data.getBookByName(this.bookTitle);
       if (this.bookData && this.bookData[0]['stars'] !== undefined) {
         this.calculateStars(this.bookData[0]['stars']);
-        this.appendStarsHtml();
+        this.generateStarIcons();
       }
     });
   }
 
   calculateStars(stars: number) {
-    this.fullStars = Math.floor(stars);
-    this.halfStars = stars % 1 >= 0.5 ? 1 : 0;
-    this.emptyStars = 5 - (this.fullStars + this.halfStars);
+    this.fullStars.set(Math.floor(stars));
+    this.halfStars.set(stars % 1 >= 0.5 ? 1 : 0);
+    this.emptyStars.set(5 - (this.fullStars() + this.halfStars()));
   }
 
-  appendStarsHtml() {
-    let container = document.querySelector('.stars-container');
-
-    for (let i = 0; i < this.fullStars; i++) {
-      const fullStar = document.createElement('span');
-      fullStar.classList.add('star', 'full-star');
-      fullStar.innerHTML = '★';
-      container?.appendChild(fullStar);
-    }
-    for (let i = 0; i < this.halfStars; i++) {
-      const halfStar = document.createElement('span');
-      halfStar.classList.add('star', 'half-star');
-      halfStar.innerHTML = '⯨';
-      container?.appendChild(halfStar);
-    }
-
-    for (let i = 0; i < this.emptyStars; i++) {
-      const emptyStar = document.createElement('span');
-      emptyStar.classList.add('star', 'empty-star');
-      emptyStar.innerHTML = '☆';
-      container?.appendChild(emptyStar);
-    }
+  generateStarIcons() {
+    const icons: string[] = [];
+    icons.push(...Array(this.fullStars()).fill('full'));
+    icons.push(...Array(this.halfStars()).fill('half'));
+    icons.push(...Array(this.emptyStars()).fill('empty'));
+    this.starIcons.set(icons);
   }
 
   incrementBooks() {
