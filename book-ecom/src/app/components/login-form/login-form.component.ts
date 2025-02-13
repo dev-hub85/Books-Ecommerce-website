f0ec264133ad2fd0b77f3c66a5e8b05b3ea71df6
Abdul Rehman <arehman8125@gmail.com>
2025-02-13 23:03:10 +0500

    Add complete responsive forms

--- book-ecom/src/app/components/login-form/login-form.component.ts
+++ book-ecom/src/app/components/login-form/login-form.component.ts
@@ -9,16 +9,29 @@ import { LoginModalService } from '../../services/loginModal/login-modal.service
 export class LoginFormComponent {
   private modalService = inject(LoginModalService);
   ngOnInit() {
-    const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
+    const signUp = document.getElementById('sign-up'),
+      signIn = document.getElementById('sign-in'),
+      loginIn = document.getElementById('login-in'),
+      loginUp = document.getElementById('login-up');
 
-    signUpButton!.addEventListener('click', () => {
-      container!.classList.add('right-panel-active');
+    signUp!.addEventListener('click', () => {
+      // Remove classes first if they exist
+      loginIn!.classList.remove('block');
+      loginUp!.classList.remove('none');
+
+      // Add classes
+      loginIn!.classList.toggle('none');
+      loginUp!.classList.toggle('block');
     });
 
-    signInButton!.addEventListener('click', () => {
-      container!.classList.remove('right-panel-active');
+    signIn!.addEventListener('click', () => {
+      // Remove classes first if they exist
+      loginIn!.classList.remove('none');
+      loginUp!.classList.remove('block');
+
+      // Add classes
+      loginIn!.classList.toggle('block');
+      loginUp!.classList.toggle('none');
     });
     this.modalService!.initializeModal('LoginSignUpModal');
   }
